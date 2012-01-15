1cce9a60489d5685c429fb812e94e5c3ee9fe6da
Dakuan <dom.barker808@gmail.com>
2012-01-15 17:52:10 +0000

    menu animation

--- dakuan/controllers/home_controller.js
+++ dakuan/controllers/home_controller.js
@@ -2,12 +2,46 @@ steal( 'jquery/controller',
        'jquery/view/ejs',
 	   'jquery/dom/form_params',
 	   'jquery/controller/view')
-	.then(function($){
+	.then('jquery/ui',function($){
 $.Controller('Dakuan.Controllers.Home',
 /** @Prototype */
 {
 	init : function(){
 		this.element.html($.View('./dakuan/views/home/init.ejs'));
+		this.small=false;
+	},
+	'#menu div click': function(el, ev){
+		
+		el.addClass('selected');
+		
+		var self= this;
+		
+		var menuElements = $('#menu div');
+		
+		if(!this.small){
+			
+			self.small = true;
+			
+			var time = 500;
+			
+			menuElements.not('.selected').children('h2').each(function(index, element){
+				$(element).fadeOut(function(){
+					$(element).parent().addClass('deselected', time, function(){
+						$(element).parent().fadeOut(function(){
+							if($('#menu div:visible').length === 1){
+								$('.selected').addClass('stage2', time);	
+							}
+						});
+					});
+				});
+			});
+		}
+		else{
+			menuElements.not('.selected').children().fadeIn();
+			menuElements.removeClass('selected').removeClass('deselected').removeClass('stage2');
+			menuElements.fadeIn();
+			self.small=false;
+		}			
 	}
 })
 })
