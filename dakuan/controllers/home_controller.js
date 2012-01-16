a98fa061bda8b676aa719e19dee19d10adb3bf5e
Dakuan <dom.barker808@gmail.com>
2012-01-16 09:51:51 +0000

    changed animations

--- dakuan/controllers/home_controller.js
+++ dakuan/controllers/home_controller.js
@@ -7,34 +7,46 @@ $.Controller('Dakuan.Controllers.Home',
 /** @Prototype */
 {
 	init : function(){
-		this.element.html($.View('./dakuan/views/home/init.ejs'));
-		this.small=false;
-	},
-	'#menu div click': function(el, ev){
 		
-		el.addClass('selected');
+		this.element.html($.View('./dakuan/views/home/init.ejs'));
+		
+		this.small=false;
+		
+		this.time = 300;
+		
+		this.easing = 'easeInOutSine'
+	},
+	
+	fadeOutDeselected: function(index, element){
+		
+		var self = this;
+		
+		$(element).fadeOut(self.time, self.easing, function(){
+			
+			$(element).parent().addClass('deselected', self.time, self.easing, function(){
+				
+				$(element).parent().fadeOut(self.time, self.easing, function(){
+					
+					if($('#menu div:visible').length === 1){
+						
+						$('.selected').addClass('stage2', self.time, self.easing, function(){
+							self.small = true;
+						});	
+					}
+				});
+			});
+		});
+	},
+	
+	switchTo: function(){
 		
 		var self= this;
 		
 		var menuElements = $('#menu div');
 		
 		if(!this.small){
-			
-			self.small = true;
-			
-			var time = 500;
-			
-			menuElements.not('.selected').children('h2').each(function(index, element){
-				$(element).fadeOut(function(){
-					$(element).parent().addClass('deselected', time, function(){
-						$(element).parent().fadeOut(function(){
-							if($('#menu div:visible').length === 1){
-								$('.selected').addClass('stage2', time);	
-							}
-						});
-					});
-				});
-			});
+								
+			menuElements.not('.selected').children('h2').each(this.callback('fadeOutDeselected'));
 		}
 		else{
 			menuElements.not('.selected').children().fadeIn();
@@ -42,6 +54,11 @@ $.Controller('Dakuan.Controllers.Home',
 			menuElements.fadeIn();
 			self.small=false;
 		}			
+	},
+	
+	'#menu div click': function(el, ev){
+		
+		el.addClass('selected', this.time, this.easing, this.callback('switchTo'));
 	}
 })
 })
