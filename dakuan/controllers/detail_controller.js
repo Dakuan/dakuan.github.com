83fb5f0198079a6867ea9c110af78cdb629c37f5
Dakuan <dom.barker808@gmail.com>
2012-02-10 19:47:42 +0000

    fixed job cell bug

--- dakuan/controllers/detail_controller.js
+++ dakuan/controllers/detail_controller.js
@@ -89,13 +89,21 @@ $.Controller('Dakuan.Controllers.Detail',
 	
 	'.jobCell click': function(el){
 
-		$('.expanded').slideUp(500, function(){
-			$('.expanded').removeClass('expanded');	
-		});
-		
-		$('.jobContent', el).slideToggle(500, function(){
-			$('.jobContent', el).addClass('expanded');
-		});
+		if(el.children('.expanded').length > 0){
+			$('.expanded', el).slideUp(500, function(){
+				$('.expanded').removeClass('expanded');	
+			});
+		}
+		else{
+			
+			$('.expanded').slideUp(500, function(){
+				$('.expanded').removeClass('expanded');	
+			});
+			
+			$('.jobContent', el).slideToggle(500, function(){
+				$('.jobContent', el).addClass('expanded');
+			});	
+		}
 	}
 })
 })
